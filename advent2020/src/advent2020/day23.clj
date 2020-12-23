22662765cdb31bb5b0d2eff634f323dd482c15b1
Neil Menne <neil.menne@bleacherreport.com>
2020-12-23 15:47:09 -0500

    use vector mutation version for part one

--- advent2020/src/advent2020/day23.clj
+++ advent2020/src/advent2020/day23.clj
@@ -3,57 +3,7 @@
 
 (def input [1 2 3 4 8 7 5 9 6])
 
-(defn play-round-v1
-  [[w x y z & xs]]
-  (let [target     (->> (range 9 0 -1)
-                        (cycle)
-                        (drop-while #(not= w %))
-                        (drop 1)
-                        (remove #{x y z})
-                        (first))]
-    (if (= target w)
-      (concat [x y z] xs [w])
-      (conj (reduce (fn [acc v]
-                     (if (= v target)
-                       (conj acc v x y z)
-                       (conj acc v)))
-                   []
-                   xs)
-            w))))
-
-(defn part-one
-  [input]
-  (let [final (loop [n 100
-                     i input]
-                (if (= 0 n)
-                  i
-                  (recur (dec n) (play-round-v1 i))))]
-    (->> (cycle final)
-         (drop-while (partial not= 1))
-         (drop 1)
-         (take 8)
-         (str/join ""))))
-
-(defn play-round-v2
-  [state]
-  (let [curr  (state 0)
-        frst  (state curr)
-        scnd  (state frst)
-        thrd  (state scnd)
-        next  (state thrd)
-        skip? #{frst scnd thrd 0}
-        dest  (loop [i (dec curr)]
-                (if (skip? i)
-                  (recur (mod (dec i) 1000001))
-                  i))
-        targ (state dest)]
-    (assoc state
-           0    next
-           curr next
-           dest frst
-           thrd targ)))
-
-(defn seq->linked-list
+(defn seq->vector
   [[frst & _ :as input]]
   (let [base (vec (repeat (inc (count input)) nil))]
     (assoc (->> input
@@ -63,13 +13,44 @@
                 (reduce #(assoc %1 (first %2) (second %2)) base))
            0 frst)))
 
+(defn play-round
+  "mutates the input vector directly"
+  [state high]
+  (let [curr  (state 0)
+        frst  (state curr)
+        scnd  (state frst)
+        thrd  (state scnd)
+        next  (state thrd)
+        skip? #{frst scnd thrd 0}
+        dest  (loop [i (dec curr)]
+                (if (skip? i)
+                  (recur (mod (dec i) (inc high)))
+                  i))
+        targ (state dest)]
+    (assoc state
+           0    next
+           curr next
+           dest frst
+           thrd targ)))
+
 (defn play-rounds
   [input times]
-  (loop [n    times
-         curr (seq->linked-list input)]
-    (if (= n 0)
-      curr
-      (recur (dec n) (play-round-v2 curr)))))
+  (let [high (apply max input)]
+    (loop [n    times
+           curr (seq->vector input)]
+      (if (= n 0)
+        curr
+        (recur (dec n) (play-round curr high))))))
+
+(defn part-one
+  [input]
+  (let [final (play-rounds input 100)]
+    (loop [curr 1
+           out []]
+      (if (= 8 (count out))
+        (str/join "" out)
+        (let [next (final curr)]
+          (recur next (conj out next)))))))
 
 (defn extend-sequence
   [seq len]
